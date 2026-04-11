2abe16e4f9d7093aebf38b060c8d09f2e9f0799e
David Vargas <dvarrui@protonmail.com>
2026-04-11 01:44:12 +0100

    v 0.17.0

--- lib/dsl/python/import.rb
+++ lib/dsl/python/import.rb
@@ -14,22 +14,22 @@ end
 
 def show_zen
   text = <<~TEXT
-  El Zen de nPython (Inspirado en Matz)
+  El Zen de nPython (Inspirado por Matz)
 
-  La felicidad del programador es el fin último.
-  Lo natural es mejor que lo explícito.
-  La libertad es mejor que la restricción.
-  La elegancia supera a la brevedad.
-  Muchos caminos son mejores que uno solo (TIMTOWTDI).
-  Si el código se lee como prosa, es buen código.
-  No castigues al programador por ser inteligente.
-  La pureza del objeto es sagrada.
-  La metaprogramación es un superpoder, úsalo con sabiduría.
-  El lenguaje debe adaptarse al humano, no el humano al lenguaje.
-  Un bloque es a menudo la respuesta.
-  Los símbolos son mejores que los strings para la identidad.
-  El principio de Menor Sorpresa es subjetivo, pero vital.
-  Si te hace sonreír al escribirlo, es Ruby.
+  01. La felicidad del programador es el fin último.
+  02. Lo natural es mejor que lo explícito.
+  03. La libertad es mejor que la restricción.
+  04. La elegancia supera a la brevedad.
+  05. Muchos caminos son mejores que uno solo (TIMTOWTDI).
+  06. Si el código se lee como prosa, es buen código.
+  07. No castigues al programador por ser inteligente.
+  08. La pureza del objeto es sagrada.
+  09. La metaprogramación es un superpoder, úsalo con sabiduría.
+  10. El lenguaje debe adaptarse al humano, no el humano al lenguaje.
+  11. Un bloque es a menudo la respuesta.
+  12. Los símbolos son mejores que los strings para la identidad.
+  13. El principio de Menor Sorpresa es subjetivo, pero vital.
+  14. Si te hace sonreír al escribirlo, es Ruby.
   TEXT
   puts text
   "(Zen)"
