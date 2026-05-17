d16072e369f5b1fb769c5377f24d75177eeca24c
David Vargas <dvarrui@protonmail.com>
2026-05-17 23:16:22 +0100

    todo python typer Persona

--- lib/dsl/python/klasses.rb
+++ lib/dsl/python/klasses.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+# Sintaxis: type(nombre_de_la_clase, bases_de_las_que_hereda, diccionario_de_atributos)
+# Persona = type('Persona', (), {'saludar': lambda self: "¡Hola!"})
+
+# Ahora puedes usarla como cualquier clase normal:
+# instancia = Persona()
+# print(instancia.saludar())  # Resultado: ¡Hola!
+# print(type(instancia))       # Resultado: <class '__main__.Persona'>
+
