d8df3cbf60e975d2a0fb17eda4c1cf765e35321e
David Vargas <dvarrui@protonmail.com>
2026-05-17 17:40:42 +0100

    fix .__

--- lib/dsl/python/types/array.rb
+++ lib/dsl/python/types/array.rb
@@ -2,18 +2,18 @@
 
 class Array
   def __
-    <<-METHODS
-    lists.__add__(             lists.__ge__(              lists.__iter__()           lists.__reversed__()       
-    lists.__class__(           lists.__getattribute__(    lists.__le__(              lists.__rmul__(            
-    lists.__class_getitem__(   lists.__getitem__(         lists.__len__()            lists.__setattr__(         
-    lists.__contains__(        lists.__getstate__()       lists.__lt__(              lists.__setitem__(         
-    lists.__delattr__(         lists.__gt__(              lists.__mul__(             lists.__sizeof__()         
-    lists.__delitem__(         lists.__hash__             lists.__ne__(              lists.__str__()            
-    lists.__dir__()            lists.__iadd__(            lists.__new__(             lists.__subclasshook__(    
-    lists.__doc__              lists.__imul__(            lists.__reduce__()                                    
-    lists.__eq__(              lists.__init__(            lists.__reduce_ex__(                                  
-    lists.__format__(          lists.__init_subclass__()  lists.__repr__() 
-    METHODS
+    puts <<-METHODS
+   list.__add__(             list.__ge__(              list.__iter__()           list.__reversed__()       
+   list.__class__(           list.__getattribute__(    list.__le__(              list.__rmul__(            
+   list.__class_getitem__(   list.__getitem__(         list.__len__()            list.__setattr__(         
+   list.__contains__(        list.__getstate__()       list.__lt__(              list.__setitem__(         
+   list.__delattr__(         list.__gt__(              list.__mul__(             list.__sizeof__()         
+   list.__delitem__(         list.__hash__             list.__ne__(              list.__str__()            
+   list.__dir__()            list.__iadd__(            list.__new__(             list.__subclasshook__(    
+   list.__doc__              list.__imul__(            list.__reduce__()                                    
+   list.__eq__(              list.__init__(            list.__reduce_ex__(                                  
+   list.__format__(          list.__init_subclass__()  list.__repr__() 
+   METHODS
   end
 end
 
--- lib/dsl/python/types/string.rb
+++ lib/dsl/python/types/string.rb
@@ -21,14 +21,14 @@ class String
   end
 
   def __
-  puts "  str.__add__(             str.__getattribute__(    str.__le__(              str.__repr__()           
-  str.__class__(           str.__getitem__(         str.__len__()            str.__rmod__(            
-  str.__contains__(        str.__getnewargs__()     str.__lt__(              str.__rmul__(            
-  str.__delattr__(         str.__getstate__()       str.__mod__(             str.__setattr__(         
-  str.__dir__()            str.__gt__(              str.__mul__(             str.__sizeof__()         
-  str.__doc__              str.__hash__()           str.__ne__(              str.__str__()            
-  str.__eq__(              str.__init__(            str.__new__(             str.__subclasshook__(    
-  str.__format__(          str.__init_subclass__()  str.__reduce__()                                   
-  atr.__ge__(              str.__iter__()           str.__reduce_ex__(    "
+  puts "   str.__add__(             str.__getattribute__(    str.__le__(              str.__repr__()           
+   str.__class__(           str.__getitem__(         str.__len__()            str.__rmod__(            
+   str.__contains__(        str.__getnewargs__()     str.__lt__(              str.__rmul__(            
+   str.__delattr__(         str.__getstate__()       str.__mod__(             str.__setattr__(         
+   str.__dir__()            str.__gt__(              str.__mul__(             str.__sizeof__()         
+   str.__doc__              str.__hash__()           str.__ne__(              str.__str__()            
+   str.__eq__(              str.__init__(            str.__new__(             str.__subclasshook__(    
+   str.__format__(          str.__init_subclass__()  str.__reduce__()                                   
+   str.__ge__(              str.__iter__()           str.__reduce_ex__(    "
   end
 end
