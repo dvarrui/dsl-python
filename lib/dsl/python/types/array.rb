dc5814085dc2eac5bf2caed3670da25c0bb26e3e
David Vargas <dvarrui@protonmail.com>
2026-05-18 17:15:03 +0100

    feat: more array methods

--- lib/dsl/python/types/array.rb
+++ lib/dsl/python/types/array.rb
@@ -2,20 +2,33 @@
 require "io/console"
 
 class Array
+  def append(item)
+    self.<<(item)
+  end
+
   def __
     names = %w[
-      list.__add__           list.__ge__           list.__iter__      list.__reversed__
-      list.__class__         list.__getattribute__ list.__le__        list.__rmul__
-      list.__class_getitem__ list.__getitem__      list.__len__       list.__setattr__
-      list.__contains__      list.__getstate__     list.__lt__        list.__setitem__
-      list.__delattr__       list.__gt__           list.__mul__       list.__sizeof__
-      list.__delitem__       list.__hash__         list.__ne__        list.__str__
-      list.__dir__           list.__iadd__         list.__new__       list.__subclasshook__
-      list.__doc__           list.__imul__         list.__reduce__    list.__eq__
-      list.__init__          list.__reduce_ex__    list.__format__    list.__init_subclass__
-      list.__repr__]
+      list.__add__(           list.__ge__(           list.__iter__()    list.__reversed__()
+      list.__class__(         list.__getattribute__( list.__le__(       list.__rmul__(
+      list.__class_getitem__( list.__getitem__(      list.__len__()     list.__setattr__(
+      list.__contains__(      list.__getstate__()    list.__lt__(       list.__setitem__(
+      list.__delattr__(       list.__gt__(           list.__mul__(      list.__sizeof__()
+      list.__delitem__(       list.__hash__(         list.__ne__(       list.__str__(
+      list.__dir__()          list.__iadd__(         list.__new__(      list.__subclasshook__(
+      list.__doc__(           list.__imul__(         list.__reduce__()  list.__eq__(
+      list.__init__(          list.__reduce_ex__()   list.__format__(   list.__init_subclass__()
+      list.__repr__()]
     Dsl::Python.puts_in_columns(names.sort)
   end
+
+  def __add__(other) = self.concat(other)
+  def __getitem__(index) = self.at(index)
+  def __len__ = self.length
+  def __sizeof__ = 40 + self.count * 8
+  # require 'objspace'
+  # mi_array = ["npython", "cli", "version"]
+  # puts ObjectSpace.memsize_of(mi_array)
+  def __str__ = self.to_s
 
   def to_s
     inspect
@@ -33,7 +46,7 @@ module Dsl
       max_screen = IO.console.winsize[1]
       max_col = max_screen / max_value
       values.each_slice(max_col) do |items|
-        words = items.map { _1 + ("(" + space * (max_value -1 - _1.length))}
+        words = items.map { _1 + space * (max_value - _1.length)}
         puts words.join 
       end
       nil
