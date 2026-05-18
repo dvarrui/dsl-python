085a845c2c10ea3e6c97bb7e1c8bfe6449635fc7
David Vargas <dvarrui@protonmail.com>
2026-05-18 09:26:22 +0100

    update

--- lib/dsl/python/repl/credits.rb
+++ lib/dsl/python/repl/credits.rb
@@ -3,9 +3,8 @@
 def credits
   text = <<~TEXT
     Thanks to Free Software, Yukuhiro Matzumoto, David Vargas Ruiz
-    and a cast of thousands for supporting nPython
-    development.  See github.com/dvarrui/dsl-python for more information.
+    and a cast of thousands for supporting nPython development.
+    See 'github.com/dvarrui/dsl-python' for more information.
   TEXT
   puts text
-  "(Credits)"
 end
